14c3b02672f84dd33a509a6fabfc8d05908a54e5
David Disley <david.disley@gmail.com>
2013-06-20 12:19:49 +0100

    Added full implementation for shuffle method.

--- src/com/company/domain/Deck.java
+++ src/com/company/domain/Deck.java
@@ -3,15 +3,32 @@ package com.company.domain;
 import java.util.*;
 
 public class Deck {
-    private List<Card> deck = new ArrayList<Card>();
+    private List<Card> deck = new ArrayList<Card>(52);
 
     public Deck() {
         Collections.addAll(deck, Card.values());
     }
 
+    private Deck(List<Card> deck) {
+        this.deck = deck;
+    }
+
     public Deck shuffle() {
-        Collections.shuffle(deck);
-        return this;
+        Random randomNumberGenerator = new Random();
+        int numberOfCardsShuffled = 0;
+        List<Card> copyOfFullDeck = new ArrayList<Card>(deck);
+        List<Card> shuffledDeck = new ArrayList<Card>();
+
+        while (numberOfCardsShuffled < 52) {
+            int randomCard = randomNumberGenerator.nextInt(52);
+
+            if (copyOfFullDeck.get(randomCard) != null) {
+                shuffledDeck.add(copyOfFullDeck.get(randomCard));
+                copyOfFullDeck.set(randomCard, null);
+                numberOfCardsShuffled++;
+            }
+        }
+        return new Deck(shuffledDeck);
     }
 
     public List<Card> cards() {
@@ -24,5 +41,20 @@ public class Deck {
         sb.append("deck=").append(deck);
         sb.append('}');
         return sb.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Deck deck1 = (Deck) o;
+
+        return !(deck != null ? !deck.equals(deck1.deck) : deck1.deck != null);
+    }
+
+    @Override
+    public int hashCode() {
+        return deck != null ? deck.hashCode() : 0;
     }
 }
